d47917c12c0cd591c57c5cbb14c41f55906a8b6a
Connor McHugh <connor.mchugh5@gmail.com>
2020-06-02 22:49:26 +1000

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
 const options = {
     client: "mysql",
     connection: {
-      host: "localhost",
-      user: "root",
-      password: "root",
-      database: "webcomputing",
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "webcomputing",
     },
-  };
+};
